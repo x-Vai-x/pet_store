6a8f78901cc5f1304f1adc804bde612ad4305fe8
Vai <vaishali2016@outlook.com>
2021-04-01 18:02:36 +0100

    modified layout

--- src/components/pages/PetShopPage.tsx
+++ src/components/pages/PetShopPage.tsx
@@ -5,6 +5,11 @@ export default function PetShopPage() {
   const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
   return (
     <>
+      <h1>Welcome to the pet store</h1>
+
+      <h2>Pets and pet owners</h2>
+
+      <h2>Pet stock</h2>
       <div className="row">
         {daysOfWeek.map((day) => (
           <DayStore day={day} />
--- src/components/partials/DayStore.tsx
+++ src/components/partials/DayStore.tsx
@@ -7,12 +7,11 @@ interface IProps {
 export default function DayStore({ day }: IProps) {
   return (
     <>
-      <div>
+      <div className="rectangle-column">
         <header>
           <h1>{day}</h1>
         </header>
       </div>
-      <div className="rectangle-column"></div>
     </>
   );
 }
